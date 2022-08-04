86b09bd8715660d5f709d3b8a9185d5940b47d30
Aftab246-jpg <108934853+Aftab246-jpg@users.noreply.github.com>
2022-08-04 13:47:53 +0530

    Add files via upload

---
+++ Demo4.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String vowel;
+Scanner sc=new Scanner(System.in);
+vowel=sc.next();
+switch(vowel)
+	{
+case "a" ||"A" :
+System.out.println("It's Vowel");
+break;
+case"e"||"E":
+System.out.println("It's Vowel");
+break;
+case "i"||"I":
+System.out.println("It's Vowel");
+break;
+case "o"||"O":
+System.out.print("It's Vowel");
+break;
+case "u"||"U":
+System.out.println("It's Vowel");
+break;
+default:
+	System.out.println("It's not a Vowel");
+	break;
+}
+	}
+
+}

419898a179afe74b8a99071ccad7e3be2767bd56
Aftab246-jpg <108934853+Aftab246-jpg@users.noreply.github.com>
2022-08-04 13:08:22 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,15 @@
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+float b=20;
+System.out.println("a+b="+(a+b));
+System.out.println("a-b="+(a-b));
+System.out.println("a*b="+(a*b));
+System.out.println("a/b="+(a/b));
+System.out.println("a%b="+(a%b));
+	}
+
+}

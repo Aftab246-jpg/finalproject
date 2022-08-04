ae5cffc2a323931ddd3e66e3fe554671e4e188fd
Aftab246-jpg <108934853+Aftab246-jpg@users.noreply.github.com>
2022-08-04 13:02:24 +0530

    Add files via upload

---
+++ S1.java
@@ -0,0 +1,15 @@
+
+public class S1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("public class");
+	}
+
+}
+class S2
+{
+	void fun() {
+		System.out.println("S2 class");
+	}
+}

0b3d5a35ffbf9907062af734aec44c639ca9383a
Aftab246-jpg <108934853+Aftab246-jpg@users.noreply.github.com>
2022-08-04 13:11:33 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,17 @@
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age=16;
+if(age>=18)
+{
+	System.out.println("Eligible for vote");
+
+}
+else {
+	System.out.println("Not Eligible for vote");
+}
+	}
+
+}

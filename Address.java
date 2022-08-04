2078338ff7e8dee03bcc245e1babda40ba62f4d4
Aftab246-jpg <108934853+Aftab246-jpg@users.noreply.github.com>
2022-08-04 11:57:28 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,13 @@
+
+public class Address {
+	String city,state,country;
+
+	public  Address(String city,String State,String country) {
+		this.city =city;
+		this.state = state;
+		this.country=country;
+		
+
+	}
+
+}

fa7f626183f76e4ed9c0af05c064739a6bf21f88
Aftab246-jpg <108934853+Aftab246-jpg@users.noreply.github.com>
2022-08-04 12:59:32 +0530

    Add files via upload

---
+++ Salary.java
@@ -0,0 +1,21 @@
+
+public class Salary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float bs=10000;
+System.out.println("BS:"+bs);
+
+float hra=bs*50/100;
+System.out.println("HRA:"+hra);
+
+float sa=bs*75/100;
+System.out.println("SA:"+sa);
+float pf=bs*12/100;
+System.out.println("PF;"+pf);
+float ns=bs+hra+sa+pf;
+System.out.println("My Salary is :"+ns);
+
+	}
+
+}

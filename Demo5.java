0a7502b9a5c64ff5f82314009c87ee5d26b73ca5
Aftab246-jpg <108934853+Aftab246-jpg@users.noreply.github.com>
2022-08-04 13:48:19 +0530

    Add files via upload

---
+++ Demo5.java
@@ -0,0 +1,19 @@
+
+public class Demo5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int countor=20000000;
+int g = 0;
+System.out.println(g);
+//int to short(data loss)
+//short shortCounter=(short)countor;
+//int to long(no data loss)
+long longCountor=countor;
+System.out.println(countor);
+char[] shortCountor = null;
+System.out.println(shortCountor);
+System.out.println(longCountor);
+	}
+
+}

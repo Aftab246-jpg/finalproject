68e9880f747dfad61a49ef2fd6c1849fc0840659
Aftab246-jpg <108934853+Aftab246-jpg@users.noreply.github.com>
2022-08-24 11:36:33 +0530

    Add files via upload

---
+++ StringSplitExample.java
@@ -0,0 +1,15 @@
+
+public class StringSplitExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="a/b/c/d";
+String[]a1=s.split("/");
+System.out.println("split string using only regex");
+for (String string:a1) {
+	System.out.println(string);
+	
+}
+	}
+
+}

4db0b5dc157167216a4c777bfec2d1f8ae7464ba
Aftab246-jpg <108934853+Aftab246-jpg@users.noreply.github.com>
2022-08-24 11:41:32 +0530

    Add files via upload

---
+++ StringCompareToExample.java
@@ -0,0 +1,18 @@
+
+public class StringCompareToExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1="abc";
+String s2="abc";
+String s3="def";
+String s4="ABC";
+System.out.println(s1.compareTo(s2));
+System.out.println(s1.compareTo(s3));
+System.out.println(s1.compareTo(s4));
+System.out.println(s1.compareToIgnoreCase(s4));
+
+
+	}
+
+}
